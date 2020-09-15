a547d84a9525391789697caf23951edb145bb207
LubinLew <lgbxyz@gmail.com>
2020-09-15 13:35:37 +0800

    update

---
+++ src/transform/src/tf_comments.c
@@ -0,0 +1,16 @@
+/* 
+ * remove comments
+ * Copyright (c) LubinLew
+ *
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <tf_common.h>
+
+
+
+uint8_t* tf_remove_comment(uint8_t* data, size_t* len)
+{
+    return data;
+}
